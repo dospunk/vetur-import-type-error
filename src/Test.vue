fdbdbfe4d4706200cc9c19a1e3b2e061c230a875
Connor Dooley <connor.dooley@azaleahealth.com>
2021-07-14 13:19:17 -0400

    fix vue and ts version

--- src/Test.vue
+++ src/Test.vue
@@ -1,18 +1,20 @@
 <template>
   <div>
-    {{ fo }}
+    {{ foo }}
   </div>
 </template>
 
 <script lang="ts">
+import { defineComponent } from 'vue'
 import type {store as storeType} from './store'
 
-export default {
+
+export default defineComponent({
   props: ['foo', 'bar'],
   setup(){
     const x = new Array<storeType>()
   }
-}
+})
 /*
 line 8: 'storeType' is declared but its value is never read.Vetur(6133)
 line 13: 'storeType' refers to a value, but is being used as a type here. Did you mean 'typeof storeType'?Vetur(2749)
